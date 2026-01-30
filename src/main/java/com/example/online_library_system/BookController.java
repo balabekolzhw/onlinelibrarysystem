3587b31baf646953bd12057fabd4958d6b52bfcd
Balabek Olzhas <moribrodd@gmail.com>
2026-01-30 10:51:07 +0500

    BookController added

--- src/main/java/com/example/online_library_system/BookController.java
+++ src/main/java/com/example/online_library_system/BookController.java
@@ -1,4 +1,19 @@
 package com.example.online_library_system;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookController {
+
+    @GetMapping("/books")
+    public List<String> getBooks() {
+        return List.of(
+                "Clean Code",
+                "Java Fundamentals",
+                "Spring Boot in Action"
+        );
+    }
 }
--- src/main/java/com/example/online_library_system/OnlineLibrarySystemApplication.java
+++ src/main/java/com/example/online_library_system/OnlineLibrarySystemApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineLibrarySystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineLibrarySystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineLibrarySystemApplication.class, args);
+    }
 }
